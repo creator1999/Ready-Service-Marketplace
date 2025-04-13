1893b2c93d612c484f9676e2f4b8625035af273c
creator1999 <bhoyarpranay3@gmail.com>
2025-04-14 03:10:39 +0530

    Initial commit

---
+++ src/main/java/com/marketplace/readyservicemarketplace/ReadyServiceMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.marketplace.readyservicemarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadyServiceMarketplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadyServiceMarketplaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marketplace/readyservicemarketplace/ReadyServiceMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketplace.readyservicemarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadyServiceMarketplaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
